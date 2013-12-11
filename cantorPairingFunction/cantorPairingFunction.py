6d335b7ee01ea0e69c7687eee5dac0098744a56b
Ike Tohru <ike.toru@dena.jp>
2013-12-11 13:45:35 +0900

    Add invertedCantorParingFunction into cantorPairingFunction.py

--- cantorPairingFunction/cantorPairingFunction.py
+++ cantorPairingFunction/cantorPairingFunction.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+import math
+
 def cantorParingFunction(x, y):
     """
     @see http://en.wikipedia.org/wiki/Pairing_function
@@ -10,9 +12,35 @@ def cantorParingFunction(x, y):
     2
     >>> cantorParingFunction(2,0)
     3
+    >>> cantorParingFunction(1,1)
+    4
+    >>> cantorParingFunction(0,2)
+    5
+    >>> cantorParingFunction(2,1)
+    7
     """
     z = ((x + y) * (x + y + 1) / 2) + y
     return z
+
+def invertedCantorParingFunction(z):
+    """
+    @see http://en.wikipedia.org/wiki/Pairing_function
+    >>> invertedCantorParingFunction(0)
+    (0, 0)
+    >>> invertedCantorParingFunction(1)
+    (1, 0)
+    >>> invertedCantorParingFunction(2)
+    (0, 1)
+    >>> invertedCantorParingFunction(3)
+    (2, 0)
+    >>> invertedCantorParingFunction(4)
+    (1, 1)
+    """
+    w = int(math.floor((math.sqrt(8*z + 1) - 1) / 2))
+    t = (w**2 + w) / 2
+    y = z - t
+    x = w - y
+    return (x,y)
 
 if __name__ == "__main__":
     import doctest
