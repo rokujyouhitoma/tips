c1eb0b6d7b48675c49f15c051216aab4211cd07e
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2019-01-12 23:12:42 +0900

    Add threadedhttpserver/test_sample_run.py

--- threadedhttpserver/test_sample_run.py
+++ threadedhttpserver/test_sample_run.py
@@ -1,5 +1,7 @@
 import json
+import unittest
 import urllib
+import urllib.request
 from http.server import SimpleHTTPRequestHandler
 
 from threadedhttpserver import ThreadedHTTPServer
@@ -34,15 +36,27 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(data), 'UTF-8'))
 
 
-def main():
-    url = 'http://localhost:8000/'
-    host = 'localhost'
-    port = 8000
-    server = ThreadedHTTPServer(host, port, request_handler=MyHTTPRequestHandler)
-    server.start()
-    print("Running: {}".format(url))
-    while True:
-        pass
+class Test(unittest.TestCase):
+    def setUp(self):
+        url = 'http://localhost:8000/'
+        host = 'localhost'
+        port = 8000
+        self.server = ThreadedHTTPServer(host, port, request_handler=MyHTTPRequestHandler)
+        print("Start...: {}".format(url))
+        self.server.start()
+        print("Running")
+
+    def tearDown(self):
+        print("Stop...")
+        self.server.stop()
+        print("Stoped")
+
+    def test_get(self):
+        with urllib.request.urlopen('http://localhost.:8000/') as f:
+            data = f.read().decode("utf8")
+            obj = json.loads(data)
+            print(obj)
+            self.assertTrue(obj.get("client").get("command") == "GET")
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
