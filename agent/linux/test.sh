fe2ed384b69580fae73dd567e6ce5226684975b4
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-12-20 09:29:20 +0900

    Update

--- agent/linux/test.sh
+++ agent/linux/test.sh
@@ -1,8 +1,10 @@
 last > result.txt
+
 last reboot
+
 # grep -ri shutdown /var/log/messages
 
-uptime
 uptime -s
+
 date +"%Y-%m-%d %I:%M:%S" --date=@$(expr `date +%s` - `cut -d "." -f 1 /proc/uptime`)
 
