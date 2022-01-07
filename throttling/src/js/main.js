84d0d47f00494f7b2c7b16d1642a46fd92f36dd9
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2022-01-07 21:16:16 +0900

    Update

--- throttling/src/js/main.js
+++ throttling/src/js/main.js
@@ -56,6 +56,34 @@ class Engine {
 
 }
 
+
+class TokenBucket {
+    constructor(max_token) {
+        this.b = max_token;
+        this.token = max_token;
+    }
+
+    add_token() {
+        if (this.token < this.b) {
+            this.token += 1;
+            return true;
+        }
+        return false;
+    }
+
+    remove_token(n) {
+        if (0 <= this.token - n) {
+            this.token -= n;
+            return true;
+        }
+        return false;
+    }
+
+    update() {
+        return this.add_token();
+    }
+}
+
 // main
 var h1 = document.createElement("h1")
 document.body.appendChild(h1);
