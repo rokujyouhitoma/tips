4b852bb8cefe78fda96271f9618c4ab442bfb96e
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2022-01-18 21:30:03 +0900

    Upadte

--- throttling/src/js/main.js
+++ throttling/src/js/main.js
@@ -8,7 +8,7 @@ class ExDate {
 class TokenBucket {
     constructor(date, hearing_time, max_token) {
         this.date = date;
-        var now = date.now();
+        let now = date.now();
         this.hearing_time = hearing_time;
         this.B = max_token;
         this.b = max_token;
@@ -17,9 +17,9 @@ class TokenBucket {
     }
 
     hearing() {
-        var now = this.date.now();
-        var delta = now - this.last_updated_at;
-        var v = Math.round(delta / this.hearing_time);
+        let now = this.date.now();
+        let delta = now - this.last_updated_at;
+        let v = Math.round(delta / this.hearing_time);
         if (0 < v && this.b < this.B) {
             this.b = (this.b + v < this.B) ? this.b + v : this.B;
             this.last_updated_at = now;
@@ -36,13 +36,13 @@ class TokenBucket {
     }
 }
 
-var token_bucket = new TokenBucket(new ExDate(), 100, 100);
+let token_bucket = new TokenBucket(new ExDate(), 100, 100);
 
-var h1 = document.createElement("h1")
+let h1 = document.createElement("h1")
 document.body.appendChild(h1);
 
 
-var button = document.createElement("button")
+let button = document.createElement("button")
 button.innerText = "hi";
 button.addEventListener("click", function(){
     token_bucket.remove_token(10);
