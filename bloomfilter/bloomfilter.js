c109d74d30d2c8f680b3289eb23a301afec8a44c
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2022-02-08 20:18:45 +0900

    Update

--- bloomfilter/bloomfilter.js
+++ bloomfilter/bloomfilter.js
@@ -2,5 +2,7 @@ class BloomFilter {
     constructor(capacity, error=0.005) {
         this.capacity = capacity;
         this.error = error;
+        this,number_bits = -capacity * Math.log(error) / Math.log(2);
     }
+
 }
