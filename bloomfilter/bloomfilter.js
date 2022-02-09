718bf9c134c599c2efc279cb8c68534d40f9d4ff
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2022-02-09 21:13:52 +0900

    Update

--- bloomfilter/bloomfilter.js
+++ bloomfilter/bloomfilter.js
@@ -2,7 +2,8 @@ class BloomFilter {
     constructor(capacity, error=0.005) {
         this.capacity = capacity;
         this.error = error;
-        this,number_bits = -capacity * Math.log(error) / Math.log(2);
+        this,num_bits = parseInt(-capacity * Math.log(error) / Math.pow(Math.log(2), 2) + 1);
+        this.num_hashes = parseInt(self.num_bits * Math.log(2) / capacity) + 1;
     }
 
 }
