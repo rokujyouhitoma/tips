ef50c0a0ae755f32e40b483216f1ba2534e29472
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2022-02-10 22:19:12 +0900

    Update

--- bloomfilter/bloomfilter.js
+++ bloomfilter/bloomfilter.js
@@ -1,9 +1,16 @@
+class BitArray {
+    constructor(initializer) {
+    }
+}
+
+
 class BloomFilter {
     constructor(capacity, error=0.005) {
         this.capacity = capacity;
         this.error = error;
         this,num_bits = parseInt(-capacity * Math.log(error) / Math.pow(Math.log(2), 2) + 1);
         this.num_hashes = parseInt(self.num_bits * Math.log(2) / capacity) + 1;
+        this.data = new BitArray(this.num_bits);
     }
 
 }
