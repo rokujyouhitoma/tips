904ea3c96dc1b9ad64b2f0a08822c19711f6cf88
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2023-04-13 21:51:23 +0900

    Update

--- rhino-example/ScriptEngineExample.java
+++ rhino-example/ScriptEngineExample.java
@@ -12,24 +12,25 @@ public class ScriptEngineExample {
     ScriptEngineManager manager = new ScriptEngineManager();
 
     List<ScriptEngineFactory> factories = manager.getEngineFactories();
-    for (ScriptEngineFactory factory : factories) {
-      System.out.println("Name : " + factory.getEngineName());
-      System.out.println("Version : " + factory.getEngineVersion());
-      System.out.println("Language name : " + factory.getLanguageName());
-      System.out.println("Language version : " + factory.getLanguageVersion());
-      System.out.println("Extensions : " + factory.getExtensions());
-      System.out.println("Mime types : " + factory.getMimeTypes());
-      System.out.println("Names : " + factory.getNames());
-    }
+    // for (ScriptEngineFactory factory : factories) {
+    //   System.out.println("Name : " + factory.getEngineName());
+    //   System.out.println("Version : " + factory.getEngineVersion());
+    //   System.out.println("Language name : " + factory.getLanguageName());
+    //   System.out.println("Language version : " + factory.getLanguageVersion());
+    //   System.out.println("Extensions : " + factory.getExtensions());
+    //   System.out.println("Mime types : " + factory.getMimeTypes());
+    //   System.out.println("Names : " + factory.getNames());
+    // }
 
     ScriptEngine engine = manager.getEngineByName("Rhino");
-    System.out.println(engine);
+    //System.out.println(engine);
     try {
+        engine.eval("print('hihi')");
         engine.eval(new FileReader("js/main.js"));
     } catch (ScriptException e) {
-        System.out.println(e);
+        e.printStackTrace();
     } catch (FileNotFoundException e) {
-        System.out.println(e);
+        e.printStackTrace();
     }
   }
 }
--- rhino-example/js/main.js
+++ rhino-example/js/main.js
@@ -1,6 +1,59 @@
 let console = {
-    "log": (msg) => { print(msg); }
+    "log": (msg) => { java.lang.System.out.println(msg); }
 };
 
 print("Hello World! by print");
 console.log("Hello World! by console.log");
+
+let test_let = "test_let";
+
+//const test_const = "test_const";
+//->throw java.lang.NullPointerException
+
+(function() {
+    // const basic support: https://mozilla.github.io/rhino/compat/engines.html#ES2015-bindings-const-basic-support
+    const test_const = "test_const";
+    console.log(test_const);
+
+    // https://mozilla.github.io/rhino/compat/engines.html#ES2015-syntax-template-literals-basic-functionality
+    console.log(`${test_const}`);
+
+    console.log(new Date());
+    console.log(new Date().getTime());
+    console.log(`${new Date()}`);
+})();
+
+//TODO: GAS JavaScript global object
+//console.log()
+
+//TODO: Google Apps Script APIs
+//PropertiesService
+//PropertiesService.getScriptProperties()
+//PropertiesService.getScriptProperties().getProperty(name)
+//PropertiesService.getScriptProperties().setProperty(key, value)
+
+//see: https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet-app?hl=ja
+/*
+  class SpreadsheetApp
+  class SpreadsheetApp.openById(id)
+*/
+
+//see: https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet?hl=ja
+//class Spreadsheet.getSheetByName()
+
+//see: https://developers.google.com/apps-script/reference/spreadsheet/sheet?hl=ja
+//class Sheet.getRange()
+//class Sheet.getLastRow()
+//class Sheet.getLastColumn()
+
+//see: https://developers.google.com/apps-script/reference/spreadsheet/range?hl=ja
+//class Range.getValues()
+
+//HtmlService
+//HtmlService.createHtmlOutput
+
+console.log(Object.keys);
+let f1 = (x) => x;
+
+//OAuth2
+//see: https://github.com/googleworkspace/apps-script-oauth2
